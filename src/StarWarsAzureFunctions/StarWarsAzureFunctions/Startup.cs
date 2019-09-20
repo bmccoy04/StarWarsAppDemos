85efb678c9028fbd62f52ae5eebdeb98dfc84202
Bryan McCoy <bmccoy@turner-industries.com>
2019-09-20 09:25:37 -0500

    Added startup class and nuget pacakges for configuration.

---
+++ src/StarWarsAzureFunctions/StarWarsAzureFunctions/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using StarWarsAzureFunctions;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+
+namespace StarWarsAzureFunctions
+{
+    public class Startup : FunctionsStartup 
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
